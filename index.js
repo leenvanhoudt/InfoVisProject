1d6e67ac1ff9b0f22653e70f0645ffc526d9555f
Leen Van Houdt <Leen>
2019-04-17 17:24:31 +0200

    datasets + onclick event

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 //map config
 var map = new Datamap({
-  scope: 'world',
+  //scope: 'world',
   element: document.getElementById('container1'),
   //set projection to Europe
   setProjection: function(element, options) { 
     var projection = d3.geo.mercator()
-      .center([10, 52])
-      .scale(700)
+      .center([20, 52])
+      .scale(800)
       .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
     var path = d3.geo.path()
       .projection(projection);                 
    return {path: path, projection: projection};
   },
-  height: 500,
+  done: function(datamap) {
+    datamap.svg.selectAll('.datamaps-subunit').on('click', function(geography) {
+        document.getElementById('countryName').innerHTML = geography.properties.name;
+        d3.json("Datasets/countries.json", function(data) {
+          var countryData = data.find(obj => {
+            return obj.name === geography.properties.name
+          })
+          //resetZoom(zoomedMap);
+          zoomToCountry(zoomedMap,countryData.latlng);
+        });
+    });
+  },
+  height: 600,
+  fills: {
+    defaultFill: '#f0af0a',
+    belgium: 'rgba(0,244,244,0.9)',
+    selectedCountry: 'red',
+  },
+  data: {
+    BEL: {fillKey: 'belgium' },
+  }
+});
+
+//zoomed map config
+var zoomedMap = new Datamap({
+  //scope: 'world',
+  element: document.getElementById('container2'),
+  //set projection to Europe
+  setProjection: function(element, options) { 
+    var projection = d3.geo.mercator()
+      .center([10, 52])
+      .scale(800)
+      .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
+    var path = d3.geo.path()
+      .projection(projection);                 
+   return {path: path, projection: projection};
+  },
+  height: 300,
   fills: {
     defaultFill: '#f0af0a',
     belgium: 'rgba(0,244,244,0.9)',
@@ -21,7 +58,19 @@ var map = new Datamap({
   data: {
     BEL: {fillKey: 'belgium' },     
   }
-})
+});
+
+function zoomToCountry(map,coordinates){
+  //TODO
+  latitude = coordinates[0];
+  longitude = coordinates[1];
+  //map.projection.center([coordinates[0],coordinates[1]]);
+  //map.svg.selectAll(".datamaps-subunits").transition().duration(750).attr("transform", "scale(1.5)translate("+coordinates[0]+","+coordinates[1]+")");
+}
+
+function resetZoom(map){
+  map.svg.selectAll(".datamaps-subunits").transition().duration(750).attr("transform", d3.zoomIdentity);
+}
 
 //arcs
 map.arc([
@@ -46,9 +95,8 @@ map.arc([
   }
 }
 ], {strokeWidth: 2});
- 
 
- //bubbles, custom popup on hover template
+//bubbles, custom popup on hover template
 map.bubbles([
  {name: 'Hot', latitude: 21.32, longitude: 5.32, radius: 10, fillKey: 'gt50'},
  {name: 'Chilly', latitude: -25.32, longitude: 120.32, radius: 18, fillKey: 'lt50'},
