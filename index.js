8c899b8ce20854b9ddd63aacfd613f95e9488f62
MartijnVR <martijn.vanrooy@telenet.be>
2019-04-20 22:43:24 +0200

    Begin heatmap

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-//erasmusdata 2010: per land -> per universiteit het aantal studenten
-d3.csv("Datasets/testdata2010.csv", function(error, csv_data) {
-var studentCountPerUniversityPerCountry = d3.nest()
-  .key(function(d) { return d.land; })
-  .key(function(d) { return d.universiteit; })
-  .rollup(function(leaves) { return leaves.length;})
-  .entries(csv_data);
-  console.log(JSON.stringify(studentCountPerUniversityPerCountry));
-});
 
 //map config
 var overviewMap = new Datamap({
   element: document.getElementById('container1'),
   //set projection to Europe
-  setProjection: function(element, options) { 
+  setProjection: function(element, options) {
     var projection = d3.geo.mercator()
       .center([10, 52])
-      .scale(800)
+      .scale(700)
       .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
     var path = d3.geo.path()
-      .projection(projection);                 
+      .projection(projection);
    return {path: path, projection: projection};
   },
   done: function(datamap) {
@@ -43,17 +34,43 @@ var overviewMap = new Datamap({
   }
 });
 
+// Build color scale
+var myColor = d3.scaleSequential()
+  .interpolator(d3.interpolateViridis)
+  .domain([1,200]);
+
+  overviewMap.updateChoropleth({
+    NL: function(){
+      d3.csv("Datasets/testdata2010.csv", function(error, csv_data) {
+      var studentCountPerUniversityPerCountry = d3.nest()
+        .key(function(d) { return d.land; })
+        .key(function(d) { return d.universiteit; })
+        .rollup(function(leaves) { return leaves.length;})
+        .entries(csv_data);
+      //  console.log(JSON.stringify(studentCountPerUniversityPerCountry));
+
+      var studentCountPerCountry = d3.nest()
+        .key(function(d) { return d.land; })
+        .rollup(function(leaves) { return leaves.length;})
+        .entries(csv_data);
+        console.log(JSON.stringify(studentCountPerCountry));
+        console.log(studentCountPerCountry[0].values);
+      });
+    return myColor(studentCountPerCountry[0].values)},
+});
+}
+
 //zoomed map config
 var zoomedMap = new Datamap({
   element: document.getElementById('container2'),
   //set projection to Europe
-  setProjection: function(element, options) { 
+  setProjection: function(element, options) {
     var projection = d3.geo.mercator()
       .center([4.7, 50.87])
       .scale(1200)
       .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
     var path = d3.geo.path()
-      .projection(projection);                 
+      .projection(projection);
    return {path: path, projection: projection};
   },
   height: 300,
@@ -63,7 +80,7 @@ var zoomedMap = new Datamap({
     selectedCountry: 'red',
   },
   data: {
-    BEL: {fillKey: 'belgium' },     
+    BEL: {fillKey: 'belgium' },
   }
 });
 
@@ -73,13 +90,13 @@ function zoomToCountry(map,coordinates){
     //scope: 'world',
     element: document.getElementById('container2'),
     //set projection to Europe
-    setProjection: function(element, options) { 
+    setProjection: function(element, options) {
       var projection = d3.geo.mercator()
         .center([coordinates[1], coordinates[0]])
         .scale(1200)
         .translate([element.offsetWidth / 2, element.offsetHeight / 2]);
       var path = d3.geo.path()
-        .projection(projection);                 
+        .projection(projection);
      return {path: path, projection: projection};
     },
     height: 300,
@@ -88,7 +105,7 @@ function zoomToCountry(map,coordinates){
       belgium: 'rgba(0,244,244,0.9)',
     },
     data: {
-      BEL: {fillKey: 'belgium' },     
+      BEL: {fillKey: 'belgium' },
     }
   });
 }
